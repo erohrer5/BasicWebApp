aef3547a6262e16c71284a3672448878bba71e66
Eric Rohrer <erohrer@andrew.cmu.edu>
2022-09-21 16:21:46 -0400

    small change to html to check if deployed

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,6 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
+                "<p>Hello/testing -Eric</p>" +
                 "<h1>Welcome!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
